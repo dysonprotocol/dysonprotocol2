fd26d12b5b971633ded2a85b77815ed93bf1024a
sybil singleton <sybil.singleton@protonmail.com>
2025-11-07 09:49:58 +0000

    fix test with unrelated denoms

--- tests/whaleswap/amm/test_cli_make_trade_missing_taker_input.py
+++ tests/whaleswap/amm/test_cli_make_trade_missing_taker_input.py
@@ -99,9 +99,10 @@ def test_cli_make_trade_missing_taker_input_accounting(
             amount = int(coin["amount"])
             expected[denom] = expected.get(denom, 0) + amount
 
-    # Verify accounting is correct
-    all_denoms = set(list(expected.keys()) + list(actual.keys()))
-    for denom in all_denoms:
+    # Verify accounting is correct for denoms used in this test only
+    # (avoid false failures from residual state left by other tests)
+    test_denoms = {foo_denom, bar_denom}
+    for denom in test_denoms:
         exp = expected.get(denom, 0)
         act = actual.get(denom, 0)
         assert (
