9df3941640372e0800abc6030b981d37bca63de2
sybil singleton <sybil.singleton@protonmail.com>
2025-11-18 04:21:15 +0000

    rename-upgrade

--- upgrades.go
+++ upgrades.go
@@ -12,7 +12,7 @@ import (
 
 // WhaleswapLeverageUpgradeName defines the on-chain upgrade name for the next
 // whaleswap migration (leverage/interest schema overhaul).
-const WhaleswapLeverageUpgradeName = "whaleswap-leverage-interest-v1"
+const WhaleswapLeverageUpgradeName = "whaleswap-v2"
 
 func (app *DysApp) RegisterUpgradeHandlers() {
 	app.Logger().Info("RegisterUpgradeHandlers: installing upgrade handlers")
