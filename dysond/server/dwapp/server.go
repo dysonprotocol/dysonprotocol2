9bc665e69c21b1c519fd9d99a058a75ecc47d019
sybil singleton <sybil.singleton@protonmail.com>
2025-12-09 00:02:28 +0000

    fix regex group for DefaultDwAppPattern

--- dysond/server/dwapp/server.go
+++ dysond/server/dwapp/server.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	ServerName                = "dwapp"
-	DefaultDwAppPattern       = `(?P<address>dys21[a-z0-9]+)|((?P<name>[a-z0-9-]+))\.`
+	DefaultDwAppPattern       = `((?P<address>dys21[a-z0-9]+)|(?P<name>[a-z0-9-]+))\.`
 	DefaultPublicHostTemplate = "{address_or_name}.localhost:1317"
 )
 
